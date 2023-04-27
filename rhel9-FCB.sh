2e8cd26595b5091481b66d5b94918d84f5376a84
johnyowoy <john-7113113@hotmail.com>
2023-04-28 00:03:10 +0800

    FCB writter

--- rhel9-FCB.sh
+++ rhel9-FCB.sh
@@ -372,12 +372,146 @@ echo "=========================="
 echo "===== NETWORK Config ====="
 echo "=========================="
 
-# IP轉送
+sysctl_conf='/etc/sysctl.conf'
+sysctl_d='/etc/sysctl.d/*.conf'
 
-# 所有網路介面禁止傳送ICMP重新導入封包
-sed -i '$a net.ipv4.conf.all.send_redirects=0' /etc/sysctl.conf
-sed -i '$a net.ipv4.conf.all.send_redirects=0' /etc/sysctl.d/*.conf
+# 108 IP轉送
+
+# 109 所有網路介面禁止傳送ICMP重新導入封包
+sed -i '$a net.ipv4.conf.all.send_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.all.send_redirects=0' ${sysctl_d}
 sysctl -w net.ipv4.conf.all.send_redirects=0
-sysctl -w net.ipv4.route.flush=1
 
-# 110 預設網路介面禁止
+# 110 預設網路介面禁止傳送ICMP重新導向封包
+sed -i '$a net.ipv4.conf.default.send_redirects=0' ${sysctl_d}
+sysctl -w  net.ipv4.conf.default.send_redirects=0
+
+# 111 所有網路介面阻擋來源路由封包
+sed -i '$a net.ipv4.conf.all.accept_source_route=0' ${sysctl_conf}
+sed -i '$a net.ipv6.conf.all.accept_source_route=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.all.accept_source_route=0' ${sysctl_d}
+sed -i '$a net.ipv6.conf.all.accept_source_route=0' ${sysctl_d}
+sysctl -w net.ipv4.conf.all.accept_source_route=0
+sysctl -w net.ipv6.conf.all.accept_source_route=0
+
+# 112 預設網路介面阻擋來源路由封包
+sed -i '$a net.ipv4.conf.default.accept_source_route=0' ${sysctl_conf}
+sed -i '$a net.ipv6.conf.default.accept_source_route=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.default.accept_source_route=0' ${sysctl_d}
+sed -i '$a net.ipv6.conf.default.accept_source_route=0' ${sysctl_d}
+sysctl -w net.ipv4.conf.default.accept_source_route=0
+sysctl -w net.ipv6.conf.default.accept_source_route=0
+
+# 113 所有網路介面阻擋ICMP重新導向封包
+sed -i '$a net.ipv4.conf.all.accept_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv6.conf.all.accept_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.all.accept_redirects=0' ${sysctl_d}
+sed -i '$a net.ipv6.conf.all.accept_redirects=0' ${sysctl_d}
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv6.conf.all.accept_redirects=0
+
+# 114 預設網路介面阻擋ICMP重新導向封包
+sed -i '$a net.ipv4.conf.default.accept_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv6.conf.default.accept_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.default.accept_redirects=0' ${sysctl_d}
+sed -i '$a net.ipv6.conf.default.accept_redirects=0' ${sysctl_d}
+sysctl -w net.ipv4.conf.default.accept_redirects=0
+sysctl -w net.ipv6.conf.default.accept_redirects=0
+
+# 115 所有網路介面阻擋安全之IMCP重新封包
+sed -i '$a net.ipv4.conf.all.secure_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.all.secure_redirects=0' ${sysctl_d}
+sysctl -w net.ipv4.conf.all.secure_redirects=0
+
+# 116 預設網路介面阻擋安全之ICMP重新導向封包
+sed -i '$a net.ipv4.conf.default.secure_redirects=0' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.default.secure_redirects=0' ${sysctl_d}
+sysctl -w net.ipv4.conf.default.secure_redirects=0
+
+# 117 所有網路介面紀錄可疑封包
+sed -i '$a net.ipv4.conf.all.log_martians=1' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.all.log_martians=1' ${sysctl_d}
+sysctl -w net.ipv4.conf.all.log_martians=1
+
+# 118 預設網路介面紀錄可疑封包
+sed -i '$a net.ipv4.conf.default.log_martians=1' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.default.log_martians=1' ${sysctl_d}
+sysctl -w net.ipv4.conf.default.log_martians=1
+
+# 119 不回應ICMP廣播要求
+sed -i '$a net.ipv4.icmp_echo_ignore_broadcasts=1' ${sysctl_conf}
+sed -i '$a net.ipv4.icmp_echo_ignore_broadcasts=1' ${sysctl_d}
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+
+# 120 忽略 造之ICMP錯誤訊息
+sed -i '$a net.ipv4.icmp_ignore_bogus_error_responses=1' ${sysctl_conf}
+sed -i '$a net.ipv4.icmp_ignore_bogus_error_responses=1' ${sysctl_d}
+sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+
+# 121 所有網路介面啟用逆向路徑過濾功能
+sed -i '$a net.ipv4.conf.all.rp_filter=1' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.all.rp_filter=1' ${sysctl_d}
+sysctl -w net.ipv4.conf.all.rp_filter=1
+
+# 122 預設網路介面啟用逆向路徑過濾功能
+sed -i '$a net.ipv4.conf.default.rp_filter=1' ${sysctl_conf}
+sed -i '$a net.ipv4.conf.default.rp_filter=1' ${sysctl_d}
+sysctl -w net.ipv4.conf.default.rp_filter=1
+
+# 123 TCP SYN cookies
+sed -i '$a net.ipv4.tcp_syncookies=1' ${sysctl_conf}
+sed -i '$a net.ipv4.tcp_syncookies=1' ${sysctl_d}
+sysctl -w net.ipv4.tcp_syncookies=1
+
+# 124 所有網路介面阻擋IPv6路由器公告訊息
+sed -i '$a net.ipv6.all.accept_ra=0' ${sysctl_conf}
+sed -i '$a net.ipv6.all.accept_ra=0' ${sysctl_d}
+sysctl -w net.ipv6.all.accept_ra=0
+
+# 125 預設網路介面阻擋IPv6路由器公告訊息
+sed -i '$a net.ipv6.conf.default.accept_ra=0' ${sysctl_conf}
+sed -i '$a net.ipv6.conf.default.accept_ra=0' ${sysctl_d}
+sysctl -w net.ipv6.conf.default.accept_ra=0
+
+sysctl -w net.ipv4.route.flush=1
+sysctl -w net.ipv6.route.flush=1
+
+# 126 DCCP協定
+touch /etc/modprobe.d/dccp.conf
+sed -i '$a install dccp /bin/true' /etc/modprobe.d/dccp.conf
+sed -i '$a blacklist dccp' /etc/modprobe.d/dccp.conf
+
+# 127 SCTP協定
+touch /etc/modprobe.d/sctp.conf
+sed -i '$a install sctp /bin/true' /etc/modprobe.d/sctp.conf
+sed -i '$a blacklist sctp' /etc/modprobe.d/sctp.conf
+
+# 128 RDS協定
+touch /etc/modprobe.d/rds.conf
+sed -i '$a install rds /bin/true' /etc/modprobe.d/rds.conf
+sed -i '$a blacklist rds' /etc/modprobe.d/rds.conf
+
+# 129 TIPC協定
+touch /etc/modprobe.d/tipc.conf
+sed -i '$a install tipc /bin/true' /etc/modprobe.d/tipc.conf
+sed -i '$a blacklist tipc' /etc/modprobe.d/tipc.conf
+
+# 130 停用無線網路介面
+nmcli radio all off
+
+# 131 網路介面混雜模式
+
+
+# 日誌與稽核
+echo "=========================="
+echo "======= LOG Config ======="
+echo "=========================="
+
+# 132 install auditd package
+dnf install audit audit-libs
+
+# 133 enable auditd service
+systemctl start auditd
+systemctl --now enable auditd
+
+# 134 稽核
